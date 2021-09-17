127330d70aa7f5be8d7bfd114a87011292b230ac
Иванов Николай Алексеевич <na_ivanov@betboom.org>
2021-09-17 16:07:40 +0300

    build version

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./index.html",
-      filename: "./index.html"
-    })
-  ]
-};
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './index.html'), // шаблон
+        filename: 'index.html', // название выходного файла
+      }),
+    ],
+    entry: {
+      index: './index.js',
+      mpegts: './mpegts.js',
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: '[name].js',
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      compress: true,
+      port: 9001,
+      host: 'localhost'
+    },
+}
